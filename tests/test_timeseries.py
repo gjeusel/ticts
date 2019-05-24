6769b86b51588e52b6e21f44e8ed9cf82f8ad66d
Guillaume Jeusel <guillaume.jeusel@engie.com>
2019-05-24 10:28:43 +0200

    Change of design

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -1,27 +1,33 @@
 from copy import deepcopy
 from unittest import mock
 
+import pytest
+
 from .conftest import CURRENT, ONEHOUR, ONEMIN
 
 
-class TestTimeSeriesGet:
+class TestTimeSeriesGetitem:
     @mock.patch("ticts.TimeSeries._get_previous")
     def test_timeseries_get_default_on_previous(self, _get_previous, smallts):
-        smallts.get(CURRENT)
+        smallts[CURRENT]
         assert _get_previous.call_count == 1
 
     def test_timeseries_get_on_previous(self, smallts):
-        assert smallts.get(CURRENT + ONEMIN) == 0
+        assert smallts[CURRENT + ONEMIN] == 0
 
     def test_timeseries_get_on_previous_out_of_left_bound(self, smallts):
-        assert smallts.get(CURRENT - ONEMIN) == 0
+        assert smallts[CURRENT - ONEMIN] == 0
 
     def test_timeseries_get_on_previous_out_of_right_bound(self, smallts):
-        assert smallts.get(CURRENT + 10 * ONEHOUR) == 9
+        assert smallts[CURRENT + 10 * ONEHOUR] == 9
+
+    def test_timeseries_get_linear_interpolate_raises(self, smallts):
+        with pytest.raises(NotImplementedError):
+            smallts[CURRENT, "linear"]
 
 
 def test_timeseries_set(smallts):
-    smallts.set(CURRENT, 1000)
+    smallts[CURRENT] = 1000
     assert smallts[CURRENT] == 1000
 
 
