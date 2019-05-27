bd0d30e65960c2af046adc6cf0252cbcbfaa9576
Guillaume Jeusel <guillaume.jeusel@engie.com>
2019-05-27 13:45:43 +0200

    Fix several bugs:
    - set_interval is not allowed when no default is set
    - copy and deepcopy take default attribute into account
    - pd.Timestamp if pandas is installed instead of Arrow

--- ticts/pandas_ext.py
+++ ticts/pandas_ext.py
@@ -1,8 +1,22 @@
+import arrow
+
 try:
     PANDAS_IS_INSTALLED = True
     import pandas as pd
 except ImportError:
     PANDAS_IS_INSTALLED = False
+
+
+def timestamp_converter(ts):
+    if PANDAS_IS_INSTALLED:
+        if isinstance(ts, arrow.Arrow):
+            ts = ts.isoformat()
+        ts = pd.Timestamp(ts)
+        if not ts.tz:
+            ts = ts.tz_localize('UTC')
+        return ts
+    else:
+        return arrow.get(ts)
 
 
 def to_dataframe(self):
