a20dc3c5f6ab00d617845a98ff0241713907a3e0
gjeusel <jeusel.guillaume@gmail.com>
2019-06-01 13:27:06 +0200

    Add more quickstart

--- docs/ipython_init.py
+++ docs/ipython_init.py
@@ -1,7 +1,26 @@
-from ticts import TimeSeries
 from datetime import timedelta
+
 import arrow
 
+from ticts import TimeSeries
 
-ts1 = arrow.get('2019-01-01')
-ts2 = arrow.get('2019-01-02')
+onehour = timedelta(hours=1)
+onemin = timedelta(minutes=1)
+dt1 = arrow.get('2019-01-01')
+dt2 = arrow.get('2019-01-02')
+
+smalldct = {
+    dt1 + onehour: 1,
+    dt1 + 3 * onehour: 3,
+    dt1 + 6 * onehour: 10,
+}
+
+smallts = TimeSeries(smalldct)
+
+otherdct = {
+    dt1 + 2 * onehour: 2,
+    dt1 + 3 * onehour: 3,
+    dt1 + 5 * onehour: 5,
+}
+
+otherts = TimeSeries(otherdct, default=0)
