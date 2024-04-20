41ad32f2c9f7fa4a4525b54c6bf8b2d4a6327273
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2024-04-20 11:57:56 +0200

    del(typing): for now

--- ticts/__init__.py
+++ ticts/__init__.py
@@ -1,17 +1,15 @@
-from __future__ import annotations
-
 from importlib import metadata
+
+__version__ = metadata.version("ticts")
 
 import pandas as pd
 
 from ticts.timeseries import TimeSeries
 
-__version__ = metadata.version("ticts")
-
 
 @pd.api.extensions.register_dataframe_accessor("to_ticts")
 class FromPandasDTtoTicTS:
-    def __init__(self, pandas_obj: pd.Series | pd.DataFrame):
+    def __init__(self, pandas_obj):
         self.obj = pandas_obj
 
     def __call__(self) -> TimeSeries:
@@ -20,7 +18,7 @@ class FromPandasDTtoTicTS:
 
 @pd.api.extensions.register_series_accessor("to_ticts")
 class FromPandasSeriestoTicTS:
-    def __init__(self, pandas_obj: pd.Series | pd.DataFrame):
+    def __init__(self, pandas_obj):
         self.obj = pandas_obj
 
     def __call__(self) -> TimeSeries:
